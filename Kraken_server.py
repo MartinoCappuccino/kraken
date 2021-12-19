4ab9f81a08f02f9ce0cb3948fd6ba955bf5f438e
MartinoCappuccino <martijnstoorvogel@gmail.com>
2021-12-19 22:37:05 +0100

    Live plotting and new strategy

--- Kraken_server.py
+++ Kraken_server.py
@@ -8,7 +8,11 @@ from datetime import datetime
 from dotenv import load_dotenv
 from threading import Thread
 from pykrakenapi import KrakenAPI
+import os
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/configs/"
+import matplotlib.pyplot as plt
 import logging
+import logging.config
 
 def load_data(Symbols):
     for Symbol in Symbols:
@@ -21,13 +25,11 @@ def load_data(Symbols):
 
                 Symbol_time_start = round(datetime.utcnow().timestamp() * 1000)
                 
-                Data = pd.DataFrame(columns = ['Symbol', 'Price', 'Time', 'Trackprice', 'Open_position'])
+                Data = pd.DataFrame(columns = ['Symbol', 'Price', 'Time', 'BuyPrice', 'SellPriceHigh', 'SellPriceLow', 'Open_position'])
                 
                 Symboldata = kraken.get_ticker_information(Symbol)
-                Data = Data.append({"Symbol" : Symbol, "Price" : Symboldata['a'][0][0], "Time" : pd.to_datetime(Symbol_time_start, unit = 'ms'), "Trackprice": 0, "Open_position": False}, ignore_index = True)
+                Data = Data.append({"Symbol" : Symbol, "Price" : Symboldata['a'][0][0], "Time" : pd.to_datetime(Symbol_time_start, unit = 'ms'), "BuyPrice": 0, "SellPriceHigh": 0, "SellPriceLow": 0, "Open_position": False}, ignore_index = True)
                 Data.Price = Data.Price.astype(float)
-                logging.info(Data)
-                logging.info("\n")
                 Data.to_sql(Symbol, sql_engine, if_exists="append", index=False)
 
                 Symbol_time_end = round(datetime.utcnow().timestamp() * 1000)
@@ -39,102 +41,126 @@ def load_data(Symbols):
             logging.info("Unable to retrieve data\n{}".format(e))
             break
         except KeyboardInterrupt:
-            logging.info("Shutting down...")
             break
 
-def strategy(Symbol, entry, lookback, qty, open_position = False):
-    Data = pd.read_sql(Symbol, sql_engine)
-    logging.info(Data)
-    print(lookback)
-    lookbackperiod = Data.iloc[-lookback:]
-    cumret = (lookbackperiod.Price.pct_change() + 1).cumprod() - 1
-    if not open_position:
-        if cumret[cumret.last_valid_index()] > entry:
-            #order = kraken.add_standard_order(pair=Symbol, type='buy', ordertype='limit', volume='0.007', price=qty, validate=False)
-            #print(order)
-            print("Bought for: {}".format(Data.Price.iloc[-1:]))
-            bought_time = round(datetime.utcnow().timestamp() * 1000)
-            open_position = True
-        else:
-            logging.info("Not buying {} yet.".format(Symbol))
-
-    
-    if open_position:
-        #sincebuy = Data.loc[Data.Time > pd.to_datetime(order['TransactTime'], unit = "ms")]
-        sincebuy = Data.loc[Data.Time > bought_time]
-        if len(sincebuy) > 1:
-            sincebuyret = (sincebuy.Price.pct_change() + 1).cumprod() - 1
-            last_entry = sincebuyret[sincebuyret.last_valid_index()]
-            if last_entry > 0.0015 or last_entry < -0.0015:
-                #order = kraken.add_standard_order(pair=Symbol, type='sell', ordertype='limit', volume='0.007', price='qty', validate=False)
-                #print(order)
-                print("Sold for: {}".format(Data.Price.iloc[-1:]))
-                open_position = False
-
-def strategyChange(Symbols, entry, quantity, lookback, open_position=False):
+def plot(Symbols, lookback):
     while True:
-        for Symbol in Symbols:
-            sql_engine = sqlalchemy.create_engine("sqlite:///"+Symbol+"stream.db")
-            try:
-                Symbol_time_start = round(datetime.utcnow().timestamp() * 1000)
+        try:
+            for Symbol in Symbols:
+                sql_engine = sqlalchemy.create_engine("sqlite:///"+Symbol+"stream.db")
+
+                plt.ion()
+                plt.show(block=False)
 
                 Data = pd.read_sql(Symbol, sql_engine)
 
                 if Data.Price.last_valid_index() >= lookback:
-                    StartPrice = Data.Price.iloc[-lookback]
-                    EndPrice = Data.Price.iloc[-1]
+                    NewData = Data.iloc[-lookback:]
+                    x = NewData["Time"]
+                    y = NewData["Price"]
+                    plt.axis([min(x), max(x), Data["Price"].min()-Data["Price"].mean()/2000, Data["Price"].max()+Data["Price"].mean()/2000])
+                    plt.plot(x,y, color='blue')
+                    print(Data.Open_position.iloc[-1])
+                    if Data.Open_position.iloc[-1] == True:
+                        plt.axhline(y=Data["SellPriceHigh"].iloc[-1], color = 'green')
+                        plt.axhline(y=Data["SellPriceLow"].iloc[-1], color = 'red')
+                        plt.axhline(y=Data["BuyPrice"].iloc[-1], color = "black")
+                    else:
+                        print(Data["BuyPrice"].iloc[-1])
+                        plt.axhline(y=Data["BuyPrice"].iloc[-1], color = 'orange')
+                    plt.gcf().autofmt_xdate()
+                    plt.title(Symbol+" price.")
+                    plt.draw()
+                    plt.pause(0.001)
 
-                    Change = EndPrice / StartPrice - 1
+                else:
+                    x = Data["Time"]
+                    y = Data["Price"]
+                    plt.axis([min(x), max(x), Data["Price"].min()-Data["Price"].mean()/2000, Data["Price"].max()+Data["Price"].mean()/2000])
+                    plt.plot(x,y, color='blue')
+                    plt.gcf().autofmt_xdate()
+                    plt.title(Symbol+" price.")
+                    plt.draw()
+                    plt.pause(0.001)
+            sleep(1)
+        except Exception as e:
+            logging.error(e)
+            break
+        except KeyboardInterrupt:
+            break
+
+def strategyChange(Symbols, entry, capital, lookback):
+    logging.info("Buying for {} euros.".format(capital))
+    logging.info("Selling when change is greater than {}%.".format(entry*100))
+    while True:
+        for Symbol in Symbols:
+            sql_engine = sqlalchemy.create_engine("sqlite:///"+Symbol+"stream.db")
+
+            try:
+                Data = pd.read_sql(Symbol, sql_engine)
+
+                if Data.Price.last_valid_index() >= lookback:
+                    open_position = Data.Open_position.iloc[-1]
+
+                    StartPrice = Data.Price.iloc[-lookback]
+                    LastPrice = Data.Price.iloc[-1]
+
+                    BuyChange = LastPrice / StartPrice - 1
 
                     if not open_position:
-                        if Change > entry:
+                        Data.BuyPrice.iloc[-1] = (entry+1) * LastPrice
+                        if BuyChange > entry:
                             #order = kraken.add_standard_order(pair=Symbol, type='buy', ordertype='limit', volume='0.007', price=qty, validate=False)
                             #print(order)
-                            logging.info("{} bought for: {}".format(Symbol, Data.Price.iloc[-1]))
+                            quantity = capital/LastPrice
+                            BuyPrice = LastPrice
+
+                            logging.info("{} {} bought for: {}".format(quantity, Symbol, LastPrice))
                             bought_time = round(datetime.utcnow().timestamp() * 1000)
-                            open_position = True
+                            
+                            Data.BuyPrice.iloc[-1] = BuyPrice
+                            Data.Open_position.iloc[-1] = True
                         else:
-                            logging.info("Not buying {} yet.\n".format(Symbol))
+                            logging.info("Not buying {} yet.".format(Symbol))
 
                     if open_position:
                         #sincebuy = Data.loc[Data.Time > pd.to_datetime(order['TransactTime'], unit = "ms")]
                         sincebuy = Data.loc[Data.Time > pd.to_datetime(bought_time, unit = "ms")]
                         if len(sincebuy) > 1:
-
-                            StartPrice = Data.Price.iloc[sincebuy]
-                            EndPrice = Data.Price.iloc[-1]
-
-                            Change = StartPrice / EndPrice - 1
-
-                            if Change > 0.0015:
+                            SellChange = BuyPrice / LastPrice - 1
+                            Data.SellPriceHigh.iloc[-1] = 1.0015 * BuyPrice
+                            Data.SellPriceHigh.iloc[-1] = 0.9985 * BuyPrice
+                            if SellChange > 0.0015:
                                 #order = kraken.add_standard_order(pair=Symbol, type='sell', ordertype='limit', volume='0.007', price='qty', validate=False)
                                 #print(order)
-                                print("Sold with profit for: {}".format(EndPrice))
-                                open_position = False
+                                SellPrice = LastPrice
+                                Profit = SellPrice - BuyPrice
+                                logging.info("{} sold with profit for: {}".format(Symbol, Profit))
+                                Data.Open_position.iloc[-1] = False
 
-                            if Change < -0.0015:
-                                logging.info("Sold with loss for: {}".format(EndPrice))
-                                open_position = False
-                
-                Symbol_time_end = round(datetime.utcnow().timestamp() * 1000)
-                Symbol_time_diff = Symbol_time_end - Symbol_time_start
-                if Symbol_time_diff < 1000:    
-                    wait_time = (1000 - Symbol_time_diff)/1000 + 0.01
-                    sleep(wait_time)
-            
+                            if SellChange < -0.0015:
+                                SellPrice = LastPrice
+                                Loss = SellPrice - BuyPrice
+                                logging.info("{} sold with loss for: {}".format(Symbol, Loss))
+                                Data.Open_position.iloc[-1] = False
+                        Data.BuyPrice.iloc[-1] = BuyPrice
+                sleep(1)
             except Exception as e:
-                logging.info("Unable to perform strategy\n{}".format(e))
+                logging.error(e)
                 break
             except KeyboardInterrupt:
-                logging.info("Shutting down...")
-                break
+                break    
 
 if __name__ == "__main__":
     api = krakenex.API()
     api.load_key('kraken_api_key.txt')
     kraken = KrakenAPI(api)
 
-    logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S', level=logging.DEBUG)
+    logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': True,
+    })
+    logging.basicConfig(format='%(asctime)s - {} - [Kraken] - [%(levelname)s] - %(message)s'.format(os.uname()[1]), datefmt='%m/%d/%Y %I:%M:%S', level=logging.DEBUG)
 
     load_dotenv(dotenv_path="kraken.env")
     SYMBOLS = json.loads(os.getenv("SYMBOLS"))
@@ -142,5 +168,6 @@ if __name__ == "__main__":
     logging.info("Tracking assets: {}\n".format(SYMBOLS))
 
     Thread(target=load_data, args=(SYMBOLS,)).start()
-    sleep(1)
-    Thread(target=strategyChange, args=(SYMBOLS, 0.1, 0.1, 60)).start()
+    sleep(2)
+    Thread(target=strategyChange, args=(SYMBOLS, 0.005, 1000, 60)).start()
+    plot(SYMBOLS, 60)
